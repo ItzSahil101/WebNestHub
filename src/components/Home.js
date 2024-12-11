a08c1618cb558f5ce9338599d780202bfc3dd0ca
SAHIL <154607992+ItzSahil101@users.noreply.github.com>
2024-12-11 06:55:18 +0545

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -123,7 +123,7 @@ const Main = () => {
               alt={item.title}
               style={cardImageStyle}
             />
-            <h4 style={{color: "blue"}}>Available</h4>
+            <h4 style={{color: "green"}}>available</h4>
             <h3>{item.title}</h3>
             <p>{item.description}</p>
             <p style={{ fontWeight: "800", color: "Highlight" }}>
